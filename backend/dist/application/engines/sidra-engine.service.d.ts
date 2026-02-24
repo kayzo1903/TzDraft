8abc322597c2e8424d3cce2303428e8ecc7a641b
kayzo1903 <kaykinangukammari@gmail.com>
2026-02-24 23:54:33 +0300

    feat:Email service

--- backend/dist/application/engines/sidra-engine.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { SidraMoveRequest, SidraMoveResponse } from './sidra-types';
-export declare class SidraEngineService {
-    getMove(request: SidraMoveRequest): Promise<SidraMoveResponse | null>;
-    private normalizeToOfficialMove;
-    private fallbackOfficialMove;
-    private executeSidraCli;
-}
--- backend/dist/application/use-cases/cancel-friend-request.use-case.d.ts
+++
@@ -1,6 +0,0 @@
-import { FriendService } from '../../domain/friend/friend.service';
-export declare class CancelFriendRequestUseCase {
-    private readonly friendService;
-    constructor(friendService: FriendService);
-    execute(requesterId: string, requesteeId: string): Promise<void>;
-}
