d47217bd28412bc5cd90b96b5b8a1417530e0c44
kayzo1903 <kaykinangukammari@gmail.com>
2026-03-20 07:23:42 +0300

    bug fixing a blog article

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.tzdraft.co.tz",
+  ),
   icons: {
     icon: "/logo/logo.png",
   },
