07e7f67c21f29a38c8fdc01f980f1788429950bb
kayzo1903 <kaykinangukammari@gmail.com>
2026-04-01 15:25:25 +0300

    fixed and updated

---
+++ backend/src/application/use-cases/join-queue.clean.spec.ts
@@ -0,0 +1,8 @@
+import { JoinQueueUseCase } from './join-queue.use-case';
+import { GameStatus, GameType } from '../../shared/constants/game.constants';
+
+describe('JoinQueueUseCase Clean', () => {
+  it('should be defined', () => {
+    expect(true).toBe(true);
+  });
+});
